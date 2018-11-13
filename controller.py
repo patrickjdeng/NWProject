80ea29a6ddd696b0d1639fbb877848558237466f
patrickjdeng <patrickjdeng@gmail.com>
2018-11-12 22:22:47 -0600

    IT finally wORKS!

--- controller.py
+++ controller.py
@@ -1,19 +1,26 @@
-from socket import *
+'''Controller'''
+#!/usr/bin/env python
+
+import socket
 
 
-def request_list():
+def connect_to_renderer():
+    """C connect to R"""
+
+    server_ip = '10.0.0.1'
+    server_port = 5300
     buffer_size = 1024
-    message = "5,2,4"  # it'll request a list from the server
-    server_port = 16000
+    message = "5;2;4"  # it'll request a list from the server
     #   prepare socket and connect
-    controller_socket = socket(AF_INET, SOCK_STREAM)
-    controller_socket.connect(('127.0.0.1', 12000))  # TODO: Discuss port
-
+    r_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    r_socket.connect((server_ip, server_port))
     #   send then wait for receive
-    controller_socket.send(message)
-    response = controller_socket.recv(buffer_size).split()
+    r_socket.send(message)
+    response = r_socket.recv(buffer_size).split(';')
     print response[0]
-    controller_socket.close()
+    r_socket.close()
+
+    
 
 
-request_list()
+connect_to_renderer()
--- rendererV2.py
+++ rendererV2.py
@@ -1,73 +1,80 @@
+'''
+renderer.py
+Zack Oldham
+11/04/2018
+The renderer broadcasts its presence and waits for a connection from a controller.
+When C connects, it sends a request to r_er to get a file 
+from the server which the r_er will then display as commanded 
+by the controller. 
+'''
 #!/usr/bin/env python
 
-#Renderer.py
-#Zack Oldham
-#11/04/2018
-#The Renderer broadcasts its presence and waits for a connection from a controller.
-#When a controller connects, it sends a request to renderer to get a file from the server which the renderer will then display as commanded by the controller. 
 
+import socket
 
-from socket import *
 
 def run_renderer():
-    buffer_size = 1024
-    server_port = 12000
-
-    rendererSocket = socket(AF_INET, SOCK_STREAM)
-    rendererSocket.bind(('', server_port))
-    rendererSocket.listen(1)
-
+    """ Run the entire renderer! Both connections!"""
     while True:
+        r_ip = ''
+        r_port = 5300
+        buffer_size = 1024
+        c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c_socket.bind((r_ip, r_port))
+        c_socket.listen(1)
         print "waiting for connection..."
-        controllerSocket, controller_addr = rendererSocket.accept()
+        c_socket, c_address = c_socket.accept()
         print "connection successful"
-        print controller_addr
+        print c_address
         #current status message from the controller
-        message = controllerSocket.recv(buffer_size).split(',') # recv_message = [type, code, data] --> data: None or [serverAddress, fileName]
-        
+        message = c_socket.recv(buffer_size).split(';') 
+        # recv_message = [type, code, data] --> data: None or [serverAddress, fileName]
+
         if message[0] == '5':     #so far so good, controller is idle so communicate accordingly
-            send_message = '6, 0, None'
-            rendererSocket.send(send_message)
+            send_message = '6;0;None'
+            c_socket.send(send_message)
+            receive_request(c_socket, r_port, buffer_size)
         else: #error occurred, terminate connection
             print "invalid controller message"
-            send_message = '10, 1, None'
-            controllerSocket.close()
-            rendererSocket.close()
+            send_message = '10;1;None'
+            c_socket.close()
+            c_socket.close()
+            return
 
+def receive_request(c_socket, renderer_port, buffer_size):
+    '''R receives file from C, requests from S'''
+    message = ['3', '', '']
+    while message[0] != 10:
+        #File to Display message from controller
+        message = c_socket.recv(buffer_size).split(';')
 
-        while message[0] != 10:
-            #File to Display message from controller
-            message = controllerSocket.recv(buffer_size).split(',')
-
-            if message[0] == 3:
-                if message[2][0] == None or message[2][1] == None:
-                    print "invalid file request: must provide server Address and file name"
-                    continue
-                else:
-                    fileSocket = socket(AF_INET, SOCK_STREAM)
-                    fileSocket.connect((message[2][0],server_port))
-                    send_message = [11, None, message[2][1]]
-                    fileSocket.send(send_message)
-                    message = fileSocket.recv(1024)
-
-                    if message[0] != 12:
-                        print "invalid response from server, try again"
-                        continue
-                    elif message[1] == 0:
-                        print "file not found"
-                        continue
-                    elif message[1] == 1:
-                        #display text file and process commands from controller as they arrive
-                        continue
-                        #at some point controller is through and issues termination message causing inner loop to end                    
-                    else:
-                        #display video file and process commands from controller as they arrive
-                        continue
-                        #at some point controller is through and issues termination message causing inner loop to end 
-                        
-
-            else:
-                print "invalid file request, try again"
+        if message[0] == 3:
+            if message[2][0] == 'None' or message[2][1] == 'None': # TODO: what is this?
+                print "invalid file request: must provide server Address and file name"
                 continue
+            else:
+                file_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                file_socket.connect((message[2][0], server_port))
+                send_message = [11, 'None', message[2][1]]
+                file_socket.send(send_message)
+                message = file_socket.recv(1024)
+
+                if message[0] != 12:
+                    print "invalid response from server, try again"
+                    continue
+                elif message[1] == 0:
+                    print "file not found"
+                    continue
+                elif message[1] == 1:
+                    # display text file and process commands from controller as they arrive
+                    continue
+                    # at some point controller is through and issues termination message causing inner loop to end                    
+                else:
+                    # display video file and process commands from controller as they arrive
+                    continue
+                    # at some point controller is through and issues termination message causing inner loop to end
+        else:
+            print "invalid file request, try again"
+            continue
             
 run_renderer()
